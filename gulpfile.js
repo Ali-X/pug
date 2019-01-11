fdee895a838f3b93eabf2a292dcc7629c44ae526
Ali-X <alina240895@yandex.ru>
2019-01-11 11:16:20 +0200

    Modify gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,35 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
 var plumber = require('gulp-plumber');
+var browserSync = require("browser-sync");
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
     return gulp.src('app/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./html'));
+        .pipe(gulp.dest('./html'))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', ["sass", "browser"], function () {
     gulp.watch('app/sass/*.scss', ['sass']);
     gulp.watch('app/pug/*.pug', ['pug']);
+    gulp.watch("src/index.html", browserSync.reload);
+});
+
+gulp.task("browser", function() {
+    browserSync({
+        server: { baseDir: "html" },
+        notify: false
+    });
 });
 
 gulp.task('pug', function () {
     return gulp.src('app/pug/*.pug')
         .pipe(plumber())
         .pipe(pug())
-        .pipe(gulp.dest('./html'));
+        .pipe(gulp.dest('./html'))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
